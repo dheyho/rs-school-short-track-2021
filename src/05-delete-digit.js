baaa377eb02f8a79aa49935673a263788a029a40
dheyho <dheyho@mail.ru>
2021-04-12 23:36:29 +0300

    feat: add 05 and 10

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,17 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('');
+  const min = Math.min.apply(null, arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (+arr[i] === min) {
+      arr.splice(i, 1);
+    }
+  }
+
+  return +arr.join('');
 }
 
 module.exports = deleteDigit;
--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,14 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      result = i;
+    }
+  }
+  return result;
 }
 
 module.exports = findIndex;
